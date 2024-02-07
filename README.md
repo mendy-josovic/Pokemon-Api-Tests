8fa9a5fb0740d3544a4398327d7d08f9a18f1100
mendy-josovic <mjosovic@j.jct.ac.il>
2024-02-07 08:58:29 +0200

    fixed the README page added some mor comments

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Pokemon-Api-Tests
-Testing pokeapi.co For Basic Scenarios 
-*Basic Structure:* 
-  using Requests package for the API request
-  using Json package for the parsing of the response
-  extracting the needed data
-  asserting to validate the response is like expected 
+Testing pokeapi.co For Basic Scenarios <br>
+
+**Basic Structure:**
+  * using Requests package for the API request<br>
+  * using Json package for the parsing of the response<br>
+  * extracting the needed data<br>
+  * asserting to validate the response is like expected<br>
+
+
--- main.py
+++ main.py
@@ -5,31 +5,30 @@ TEST_URLS = {'typeurl': 'https://pokeapi.co/api/v2/type', 'firetype': 'https://p
 TOP_5_WEIGHTS = {'charizard-gmax': 10000, 'cinderace-gmax': 10000, 'coalossal-gmax': 10000, 'centiskorch-gmax': 10000, 'groudon-primal': 9997}
 
 
-def test_varify_type_count():     #using requests and json packges to extract the response from the first API
+def test_verify_type_count(): # using requests and json packages to extract the response from the first API
     response = json.loads(requests.get(TEST_URLS['typeurl']).content)
-    print(len(response.get('results')))
-    assert response.get('count') == len(response.get('results'))
+    assert response.get('count') == len(response.get('results'))# validating the result count is equal to the count field
 
 
 def test_validate_fire_pokemon():
     response = json.loads(requests.get(TEST_URLS['firetype']).content)
     pokemons = response.get('pokemon')
-    pokemon_names =[entry['pokemon']['name'] for entry in pokemons]
-    assert 'charmander' in pokemon_names
-    assert 'bulbasaur' not in pokemon_names
+    pokemons_names =[entry['pokemon']['name'] for entry in pokemons]# getting all pokemon names
+    assert 'charmander' in pokemons_names
+    assert 'bulbasaur' not in pokemons_names
 
 def test_validate_top_5():
     response = json.loads(requests.get(TEST_URLS['firetype']).content)
     pokemons = response.get('pokemon')
-    pokemon_urls =[{'name': entry['pokemon']['name'], 'url': entry['pokemon']['url']} for entry in pokemons]
+    pokemon_urls =[{'name': entry['pokemon']['name'], 'url': entry['pokemon']['url']} for entry in pokemons] # getting all pokemon names and urls ia dict
     names_and_weights ={}
-    for url in pokemon_urls:
+    for url in pokemon_urls: # getting all weights of pokemons
         weight = get_weight(url['url'])
         names_and_weights.update({url['name']: weight})
 
 
-    sorted_dict = dict(sorted(names_and_weights.items(), key=lambda x: x[1], reverse=True))
-    top_5 = dict(list(sorted_dict.items())[:5])
+    sorted_dict = dict(sorted(names_and_weights.items(), key=lambda x: x[1], reverse=True))# sorting descending the list by the weights
+    top_5 = dict(list(sorted_dict.items())[:5]) # getting the first 5
     assert top_5 == TOP_5_WEIGHTS
 
 
